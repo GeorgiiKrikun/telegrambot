16b5ab1c64a7deb1de1ad06ac6ae8daa7312b8a9
Georgii <georgii.krikun@gmail.com>
2018-09-19 00:42:48 +0200

    Initial commit

---
+++ receiver.py
@@ -0,0 +1,67 @@
+﻿from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+import logging
+
+updater = Updater(token='695112427:AAGlDG_vmb9UdRxKxuCvCuw5ba8ISdFahBQ')
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+#/start Handling
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="SOBAKI ETO PROSTO BLYAD OHUENNAYA TEMA")
+
+start_handler = CommandHandler('start',start)
+
+dispatcher.add_handler(start_handler)
+
+#MessageHandler
+
+def messagesave(bot, update):
+    if ((update.effective_user.username != "goglike") & (update.effective_user.username != "Leenza") & (update.effective_user.username != "kireenkov")):
+        bot.send_message(chat_id=update.message.chat_id, text="NAHUI POSHEL, ti ne goglike")
+        return
+    current_number_file=open("current_number","r")
+    current_number=current_number_file.read()
+    current_number_file.close()
+
+    if (len(update.message.photo) != 0):
+ 	desc=open("description/"+current_number,"w")
+        desc.write("photo\n")
+        incomephotos=update.message.photo
+    	incomefile=incomephotos[len(incomephotos)-1].get_file();
+    	incomefile.download(custom_path="pictures/"+current_number+".jpg")
+        bot.send_message(chat_id=update.message.chat_id, text="Photo saved")
+        if (update.message.caption != None):
+            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
+            desc.write("cap")
+            cap=open("text/"+current_number,"w")
+            cap.write(update.message.caption)
+            cap.close()
+            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
+        else:
+            desc.write("nocap")
+        desc.close()
+    if (update.message.text != None):
+        desc=open("description/"+current_number,"w")
+        desc.write("text\n")
+        desc.close()
+        cap=open("text/"+current_number,"w")
+        cap.write(update.message.text)
+        cap.close()
+        bot.send_message(chat_id=update.message.chat_id, text="Text saved")
+    
+
+    current_number_file=open("current_number","w")
+    current_number_file.write(str(int(current_number)+1))
+    
+    print(update.effective_user.username)
+    
+#photo_handler=MessageHandler(Filters.photo,imagesave)
+message_handler=MessageHandler(Filters.all,messagesave)
+
+dispatcher.add_handler(message_handler)
+updater.start_polling()
---
+++ sender.py
@@ -0,0 +1,48 @@
+﻿from telegram.ext import Updater
+#from telegram.ext import Bot
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from datetime import datetime,time,timedelta
+#from threading import Timer
+import logging
+import subprocess
+import time as tt
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+N=3
+
+times = [time(10,40,0),time(10,40,30),time(10,41,0),time(10,41,45)]
+
+#Sending function
+
+def imagesend():
+	pictures=subprocess.check_output("ls -t *.jpg", shell=True).split('\n')
+	picturestr=pictures[len(pictures)-2]
+	picture=open(picturestr,"r")
+	updater = Updater(token='473906094:AAHPLdTeCEPLrPPxOLu2mUn9T_Wp1Oi9YaY')
+	bot=updater.bot
+	bot.send_photo(chat_id="@temporalgog", photo=picture)
+	print("rm -v "+picturestr)
+	subprocess.call(["rm","-v",picturestr])
+
+def timetofulltime(t):
+	return t.hour*3600+t.minute*60+t.second
+
+NUtimes=[timetofulltime(i) for i in times]
+
+while True:
+	temp=-1;
+	currenttime=timetofulltime(datetime.now().time())
+	for i in range(len(NUtimes)):
+		if currenttime<NUtimes[i]:
+			temp=i;
+			break
+	if (temp > -1):
+		print("next iteration in: ",NUtimes[temp]-currenttime);
+		tt.sleep(NUtimes[temp]-currenttime);
+		imagesend();
+	else:
+		print("Putting myself asleep till midnight");
+		tt.sleep(24*60*60-currenttime+1);
+
