d2acfb5fcff41d12ec6f810b68f0f7828190891e
Georgii <georgii.krikun@gmail.com>
2018-10-28 15:22:49 +0100

    Added an option to send media groups

--- receiver.py
+++ receiver.py
@@ -11,88 +11,7 @@ def start(bot, update):
 #/stop react
 def stop(num):
     subprocess.call(["rm","-v","description/"+num])                
-           
-#MessageHandler
-def messagesave(bot, update):
-    if ((update.effective_user.username != "goglike") & (update.effective_user.username != "Leenza") & (update.effective_user.username != "kireenkov")):
-        bot.send_message(chat_id=update.message.chat_id, text="NAHUI POSHEL, ti ne goglike")
-        return
-    current_number_file=open("current_number","r")
-    current_number=current_number_file.read()
-    current_number_file.close()
-
-    if (update.message.text != None ):
-        comarr = update.message.text.split(' ')
-        if ((comarr[0] == '/stop') & (len(comarr) > 1) ):
-            stop(comarr[1])
-            bot.send_message(chat_id=update.message.chat_id, text="Task number "+comarr[1] +" stopped")
-            return
-    
-    bot.send_message(chat_id=update.message.chat_id, text="Your task number = "+ current_number+". Use it if you want to stop bot from posting it in form: /stop "+current_number)
-    
-    if (update.message.media_group_id != None):
-        print(update.message.media_group_id)
-    
-    if (len(update.message.photo) != 0):
-        desc=open("description/"+current_number,"w")
-        desc.write("photo\n")
-        incomephotos=update.message.photo
-        incomefile=incomephotos[len(incomephotos)-1].get_file();
-        incomefile.download(custom_path="pictures/"+current_number+".jpg")
-        bot.send_message(chat_id=update.message.chat_id, text="Photo saved")
-        if (update.message.caption != None):
-            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-    if (update.message.text != None):
-        desc=open("description/"+current_number,"w")
-        desc.write("text\n")
-        desc.close()
-        cap=open("text/"+current_number,"w")
-        cap.write(update.message.text)
-        cap.close()
-        bot.send_message(chat_id=update.message.chat_id, text="Text saved")
-    if (update.message.document != None):
-        print("document")
-        desc=open("description/"+current_number,"w")
-        desc.write("doc\n")
-        incomefile=update.message.document.get_file();
-        incomefile.download(custom_path="docs/"+current_number+mimetypes.guess_extension(update.message.document.mime_type))
-        bot.send_message(chat_id=update.message.chat_id, text=mimetypes.guess_extension(update.message.document.mime_type)+" saved")
-        desc.write(mimetypes.guess_extension(update.message.document.mime_type)+'\n')
-        if (update.message.caption != None):
-            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-        desc.close()
-    if (update.message.video != None):
-        print("video")
-        desc=open("description/"+current_number,"w")
-        desc.write("video\n")
-        incomefile=update.message.video.get_file();
-        incomefile.download(custom_path="videos/"+current_number)
-        bot.send_message(chat_id=update.message.chat_id, text="video saved")
-        #desc.write(mimetypes.guess_extension(update.message.document.mime_type)+'\n')
-        if (update.message.caption != None):
-            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-        desc.close()
-
-    current_number_file=open("current_number","w")
-    current_number_file.write(str(int(current_number)+1))
-    
-    print(update.effective_user.username)
-    
+   
 def messagesave2(bot, update):
     if ((update.effective_user.username != "goglike") & (update.effective_user.username != "Leenza") & (update.effective_user.username != "kireenkov")):
         bot.send_message(chat_id=update.message.chat_id, text="NAHUI POSHEL, ti ne goglike")
@@ -110,65 +29,71 @@ def messagesave2(bot, update):
     
     bot.send_message(chat_id=update.message.chat_id, text="Your task number = "+ current_number+". Use it if you want to stop bot from posting it in form: /stop "+current_number)
     
-    if (update.message.media_group_id != None):
-        print(update.message.media_group_id)
- 
-    if (len(update.message.photo) != 0):
-        desc=open("description/"+current_number,"w")
-        desc.write("photo\n")
-        incomephotos=update.message.photo
-        incomefile=incomephotos[len(incomephotos)-1].get_file();
-        bot.send_message(chat_id=update.message.chat_id, text="Photo saved")
-        if (update.message.caption != None):
-            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-        desc.write('\n'+incomefile.file_id)
-        desc.close()
-    if (update.message.document != None):
-        desc=open("description/"+current_number,"w")
-        desc.write("doc\n")
-        incomefile=update.message.document.get_file();
-        bot.send_message(chat_id=update.message.chat_id, text="file saved")
-        if (update.message.caption != None):
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-        desc.write('\n'+incomefile.file_id)
-        desc.close()
-    if (update.message.video != None):
-        print("video")
-        desc=open("description/"+current_number,"w")
-        desc.write("video\n")
-        incomefile=update.message.video.get_file();
-        bot.send_message(chat_id=update.message.chat_id, text="video saved")
-        #desc.write(mimetypes.guess_extension(update.message.document.mime_type)+'\n')
-        if (update.message.caption != None):
-            #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
-            desc.write("cap")
-            cap=open("text/"+current_number,"w")
-            cap.write(update.message.caption)
-            cap.close()
-            bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
-        desc.write('\n'+incomefile.file_id)
-        desc.close()
-        
-    if (update.message.text != None):
-        desc=open("description/"+current_number,"w")
-        desc.write("text\n")
-        desc.close()
-        cap=open("text/"+current_number,"w")
-        cap.write(update.message.text)
-        cap.close()
-        bot.send_message(chat_id=update.message.chat_id, text="Text saved")
-
-    current_number_file=open("current_number","w")
-    current_number_file.write(str(int(current_number)+1))
+    if (update.message.media_group_id == None):    
+            if (update.message.text != None):
+                desc=open("description/"+current_number,"w")
+                desc.write("text\n")
+                desc.close()
+                cap=open("text/"+current_number,"w")
+                cap.write(update.message.text)
+                cap.close()
+                bot.send_message(chat_id=update.message.chat_id, text="Text saved")  
+            elif (len(update.message.photo) != 0):
+                desc=open("description/"+current_number,"w")
+                desc.write("photo\n")
+                incomephotos=update.message.photo
+                incomefile=incomephotos[len(incomephotos)-1].get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="Photo saved")
+            elif ( update.message.document != None):
+                desc=open("description/"+current_number,"w")
+                desc.write("doc\n")
+                incomefile=update.message.document.get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="file saved")
+            elif ( update.message.video != None ):
+                desc=open("description/"+current_number,"w")
+                desc.write("video\n")
+                incomefile=update.message.video.get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="video saved")
+            if (update.message.caption != None):
+                #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
+                desc.write("cap")
+                cap=open("text/"+current_number,"w")
+                cap.write(update.message.caption)
+                cap.close()
+                bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
+            desc.write('\n'+incomefile.file_id)
+            desc.close()
+            current_number_file=open("current_number","w")
+            current_number_file.write(str(int(current_number)+1))        
+    else:
+            if (len(update.message.photo) != 0):
+                desc=open("description/"+current_number,"w")
+                desc.write("photo\n")
+                incomephotos=update.message.photo
+                incomefile=incomephotos[len(incomephotos)-1].get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="Photo saved")
+            elif ( update.message.document != None):
+                desc=open("description/"+current_number,"w")
+                desc.write("doc\n")
+                incomefile=update.message.document.get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="file saved")
+            elif ( update.message.video != None ):
+                desc=open("description/"+current_number,"w")
+                desc.write("video\n")
+                incomefile=update.message.video.get_file();
+                bot.send_message(chat_id=update.message.chat_id, text="video saved")
+            if (update.message.caption != None):
+                #bot.send_message(chat_id=update.message.chat_id, text="caption "+update.message.caption)
+                desc.write("cap")
+                cap=open("text/"+current_number,"w")
+                cap.write(update.message.caption)
+                cap.close()
+                bot.send_message(chat_id=update.message.chat_id, text="Caption saved")
+            desc.write('\n'+incomefile.file_id)
+            desc.write('\n'+update.message.media_group_id)
+            desc.close()
+            current_number_file=open("current_number","w")
+            current_number_file.write(str(int(current_number)+1))
     
     print(update.effective_user.username)
     
--- sender.py
+++ sender.py
@@ -2,17 +2,23 @@
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
+#import telegram.InputMediaPhoto as IMP
+#import telegram.InputMediaDocument as IMD
+#import telegram.InputMediaVideo as IMV
+import telegram as tel
+from pathlib import Path
 import mimetypes
 import subprocess
 from datetime import datetime,time,timedelta
 import time as tt
 import threading
 
+#send non media group post
+
+
+
 #sender send function
 def sendstuff(sender,chat):
-        #current_number_file=open("current_number","r")
-        #current_number=current_number_file.read()
-        #current_number_file.close()
         last_file=subprocess.check_output("ls -t description/", shell=True).decode('unicode_escape').split('\n')
         if (len(last_file) <= 1):
                 print("no files")
@@ -28,47 +34,80 @@ def sendstuff(sender,chat):
                 return
         updater = Updater(token=sender)
         bot=updater.bot
-        if (desc[0]=='text'):
-                caption_file=open("text/"+laststr,"r")
-                caption=caption_file.read()
-                caption_file.close()
-                bot.send_message(chat_id=chat,text=caption)
-        if ((desc[0]=='photo') & (desc[1] != 'cap')):
-                bot.send_photo(chat_id=chat, photo=desc[2])
-        if ((desc[0]=='photo') & (desc[1] == 'cap')):
-                caption_file=open("text/"+laststr,"r")
-                caption=caption_file.read()
-                caption_file.close()
-                bot.send_photo(chat_id=chat, photo=desc[2],caption=caption)
-        if ((desc[0]=='doc') & (desc[1] != 'cap')):
-                bot.send_document(chat_id=chat, document=desc[2])
-        if ((desc[0]=='doc') & (desc[1] == 'cap')):
-                caption_file=open("text/"+laststr,"r")
-                caption=caption_file.read()
-                caption_file.close()
-                bot.send_document(chat_id=chat, document=desc[2],caption=caption)
-        if ((desc[0]=='video') & (desc[1] != 'cap')):
-                bot.send_video(chat_id=chat, video=desc[2])
-        if ((desc[0]=='video') & (desc[1] == 'cap')):
-                caption_file=open("text/"+laststr,"r")
-                caption=caption_file.read()
-                caption_file.close()
-                bot.send_video(chat_id=chat, video=desc[2], caption=caption)
-        
-        #if (len(desc)==3):        
-        #        if ((desc[0]=='doc') & (desc[2] != 'cap')):
-        #                doc=open("docs/"+laststr+desc[1],'rb')
-        #                bot.send_document(chat_id=chat, document=doc)
-        #                doc.close();
-        #        if ((desc[0]=='doc') & (desc[2] == 'cap')):
-        #                doc=open("docs/"+laststr+desc[1],'rb')
-        #                caption_file=open("text/"+laststr,"r")
-        #                caption=caption_file.read()
-        #                caption_file.close()
-        #                bot.send_document(chat_id=chat, document=doc,caption=caption)
-        #                doc.close();
-        subprocess.call(["rm","-v","description/"+laststr])                
-
+        if (len(desc)<=3):
+                if (desc[0]=='text'):
+                        caption_file=open("text/"+laststr,"r")
+                        caption=caption_file.read()
+                        caption_file.close()
+                        bot.send_message(chat_id=chat,text=caption)
+                if ((desc[0]=='photo') & (desc[1] != 'cap')):
+                        bot.send_photo(chat_id=chat, photo=desc[2])
+                if ((desc[0]=='photo') & (desc[1] == 'cap')):
+                        caption_file=open("text/"+laststr,"r")
+                        caption=caption_file.read()
+                        caption_file.close()
+                        bot.send_photo(chat_id=chat, photo=desc[2],caption=caption)
+                if ((desc[0]=='doc') & (desc[1] != 'cap')):
+                        bot.send_document(chat_id=chat, document=desc[2])
+                if ((desc[0]=='doc') & (desc[1] == 'cap')):
+                        caption_file=open("text/"+laststr,"r")
+                        caption=caption_file.read()
+                        caption_file.close()
+                        bot.send_document(chat_id=chat, document=desc[2],caption=caption)
+                if ((desc[0]=='video') & (desc[1] != 'cap')):
+                        bot.send_video(chat_id=chat, video=desc[2])
+                if ((desc[0]=='video') & (desc[1] == 'cap')):
+                        caption_file=open("text/"+laststr,"r")
+                        caption=caption_file.read()
+                        caption_file.close()
+                        bot.send_video(chat_id=chat, video=desc[2], caption=caption)
+                subprocess.call(["rm","-v","description/"+laststr])                
+        elif (len(desc)==4):
+                numlist=[]
+                desclist=[]
+                while(len(desc)==4):
+                        numlist.append(laststr)
+                        desclist.append(desc)
+                        
+                        laststr=str(int(laststr)+1)
+                        temppath=Path("description/"+laststr)
+                        if temppath.is_file():
+                                description_file=open("description/"+laststr,"r")
+                                desc=description_file.read().split('\n')
+                                description_file.close()
+                        else:
+                                break
+                
+                print(numlist,desclist)
+                inputmedia=[]
+                for i in range(len(numlist)):
+                        desc=desclist[i]
+                        if ((desc[0]=='photo') & (desc[1] != 'cap')):
+                                inputmedia.append(tel.InputMediaPhoto(desc[2]))
+                        if ((desc[0]=='photo') & (desc[1] == 'cap')):
+                                caption_file=open("text/"+numlist[i],"r")
+                                caption=caption_file.read()
+                                caption_file.close()
+                                inputmedia.append(tel.InputMediaPhoto(desc[2],caption=caption))
+                        if ((desc[0]=='doc') & (desc[1] != 'cap')):
+                                inputmedia.append(tel.InputMediaDocument(desc[2]))
+                        if ((desc[0]=='doc') & (desc[1] == 'cap')):
+                                caption_file=open("text/"+numlist[i],"r")
+                                caption=caption_file.read()
+                                caption_file.close()
+                                inputmedia.append(tel.InputMediaDocument(desc[2],caption=caption))
+                        if ((desc[0]=='video') & (desc[1] != 'cap')):
+                                inputmedia.append(tel.InputMediaVideo(desc[2]))
+                        if ((desc[0]=='video') & (desc[1] == 'cap')):
+                                caption_file=open("text/"+numlist[i],"r")
+                                caption=caption_file.read()
+                                caption_file.close()
+                                inputmedia.append(tel.InputMediaVideo(desc[2],caption=caption))
+                print(len(inputmedia))
+                bot.send_media_group(chat_id=chat,media=inputmedia)
+                for i in numlist:
+                        subprocess.call(["rm","-v","description/"+i])
+                                
 def timetofulltime(t):
 	return t.hour*3600+t.minute*60+t.second
 
